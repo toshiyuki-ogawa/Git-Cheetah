41d2e0468b36deaacc407d22c5427e18909f8465
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-03-23 14:28:01 -0500

    Detach Git Bash
    When called from the Windows Explorer, we must not wait for the
    stdout/stderr of Git Bash.
    
    A recent change made for FarManager makes Git Cheetah capture
    stderr/stdout even when we are not interested in it, to avoid cluttering
    FarManager's precious console. Due to this workaround, Git Bash makes the
    Explorer -- Git Cheetah's primary intended consumer -- hang.
    
    Since Git Bash does not output anything to the calling console, let's just
    force the detached mode by avoiding the FarManager workaround *just* for
    Git Bash.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- common/exec.c
+++ common/exec.c
@@ -54,7 +54,7 @@ int exec_program_v(const char *working_directory,
 		return -1;
 	}
 
-	if (!output || !error_output)
+	if ((!output || !error_output) && !(DETACHMODE & flags))
 		devnull = open("/dev/null", O_WRONLY);
 
 	s1 = dup(1);
@@ -65,7 +65,7 @@ int exec_program_v(const char *working_directory,
 		dup2(fdout[1], 1);
 
 		flags |= WAITMODE;
-	} else {
+	} else if (devnull) {
 		dup2(devnull, 1);
 	}
 
@@ -79,7 +79,7 @@ int exec_program_v(const char *working_directory,
 		dup2(fderr[1], 2);
 
 		flags |= WAITMODE;
-	} else {
+	} else if (devnull) {
 		dup2(devnull, 2);
 	}
 	close(devnull);
